d3e6853c260a3ce402e8c24cfcff156a0feb3663
MasterJeon <165636159+MasterJeon@users.noreply.github.com>
2024-08-06 12:10:53 +0530

    on boarding pages. a single edit have to be done

--- lib/welcome_pages/onboarding.dart
+++ lib/welcome_pages/onboarding.dart
@@ -3,6 +3,8 @@ import 'package:e_shuttle/welcome_pages/wScreen1.dart';
 import 'package:e_shuttle/welcome_pages/wScreen2.dart';
 import 'package:e_shuttle/welcome_pages/wScreen3.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,15 +32,60 @@ class OnBoardingScreen extends StatefulWidget{
 
 class _OnBoardingScreenState extends State<OnBoardingScreen>{
 
+  PageController pageController = PageController();
+  String buttonText = "Skip";
+  int currentPageIndex = 0; 
+
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: Colors.white,
-      body: PageView(
+      body: Stack(
         children: [
-          Screen1(),
-          Screen2(),
-          Screen3(),
+          PageView(
+            controller: pageController,
+            onPageChanged:(index){
+              currentPageIndex = index;
+              if (index == 3){
+                buttonText = "Finish";
+              }else{
+                buttonText = "Skip";
+              }
+              setState(() {
+                
+              });
+            } ,
+            children: const [
+              Screen1(),
+              Screen2(),
+              Screen3(),
+            ],
+          ),
 
+          Container(
+            alignment: const Alignment(0, 0.8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: const Text(buttonText)
+                ),
+                SmoothPageIndicator(
+                  controller: pageController, 
+                  count: 3
+                ),
+
+                currentPageIndex == 3 ? const SizedBox (width: 10 ,): GestureDetector(
+                  onTap: (){
+                    pageController.nextPage(duration: Duration(milliseconds:500 ), curve: Curves.easeIn);
+                  },
+                  child: const Text("Next")
+                )           
+              ],
+            ),
+          )
         ],
       )
     );
