857fc6c27fafe5a799ea10db3df74e2ed3117efe
Senuda Weliwatta <senudaweli@gmail.com>
2024-09-02 21:43:52 +0530

    routesFinalUpdate

--- lib/testingMap/routetest.dart
+++ lib/testingMap/routetest.dart
@@ -25,19 +25,26 @@ class RouteDetails {
 }
 
 
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
 
   final LatLng _center = const LatLng(6.81750000, 79.89027778);
-  
-  void _onMapCreated(GoogleMapController controller){
-    mapController = controller; 
-  } 
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   final Set<Polyline> _polylines = {};
-  final List<LatLng> _routeCoords = [];
 
   final List<RouteDetails> _routes = [
+    RouteDetails(
+      startLocation: LatLng(6.81750000, 79.89027778),
+      endLocation: LatLng(6.723170703502162, 80.06583043717407),
+      waypoints: [
+        LatLng(6.710187124161938, 79.90792773892063),
+        LatLng(6.713937790150642, 79.98895190786376),
+      ],
+    ),
     RouteDetails(
       startLocation: LatLng(6.81750000, 79.89027778),
       endLocation: LatLng(6.892324725865243, 80.08854102823662),
@@ -48,14 +55,37 @@ class _MyAppState extends State<MyApp>{
       ],
     ),
     RouteDetails(
-      startLocation: LatLng(6.841110311554549, 79.96538119310712),
-      endLocation: LatLng(7.0319529504541665, 80.02978339087662),
+      startLocation: LatLng(6.81750000, 79.89027778),
+      endLocation: LatLng(7.140999095293039, 80.0940343340502),
       waypoints: [
-        LatLng(6.850763264031758, 79.92749206117588),
-        LatLng(6.908986729283659, 79.93080459154916),
+        LatLng(6.8475074321400395, 79.92684536855644),
+        LatLng(6.9073562249883995, 79.93205484777869),
+        LatLng(6.929096638160336, 79.9827753536656),
+        LatLng(6.990469786485393, 80.01507697617508),
+        LatLng(7.031678090285276, 80.02916747125509),
+        LatLng(7.086972685701563, 80.0316414091988),
       ],
     ),
-    // Add more routes as needed
+    RouteDetails(
+      startLocation: LatLng(6.81750000, 79.89027778),
+      endLocation: LatLng(6.7260981347864925, 80.03444020794345),
+      waypoints: [
+        LatLng(6.801683003645485, 79.92284226523292),
+        LatLng(6.784431719653115, 79.97820705905943),
+        LatLng(6.754860677381756, 80.01516740570327),
+        LatLng(6.735410206202415, 80.02831908725203),
+      ],
+    ),
+    RouteDetails(
+      startLocation: LatLng(6.81750000, 79.89027778),
+      endLocation: LatLng(7.2058542889936605, 79.85043375277819),
+      waypoints: [
+        LatLng(6.8395726845980915, 79.88464910194882),
+        LatLng(6.863026997578493, 79.9014690823925),
+        LatLng(6.966050944792678, 79.88248543320111),
+        LatLng(7.065097933562652, 79.90383315349028),
+      ],
+    ),
   ];
 
   @override
@@ -63,39 +93,39 @@ class _MyAppState extends State<MyApp>{
     super.initState();
     _getRoute();
   }
-  void _getRoute() async{
+
+  void _getRoute() async {
     String googleAPIKey = "AIzaSyBRDV8VbzhAJvMyfWuqpObUKGOFBZ_kcgs";
     final directions = GoogleMapsDirections(apiKey: googleAPIKey);
-    
+
     for (int i = 0; i < _routes.length; i++) {
-    final routeDetails = _routes[i];
-    final waypoints = routeDetails.waypoints.map((stop) => Waypoint(
-      value: '${stop.latitude},${stop.longitude}',
-    )).toList();
+      final routeDetails = _routes[i];
+      final waypoints = routeDetails.waypoints.map((stop) => Waypoint(
+            value: '${stop.latitude},${stop.longitude}',
+          )).toList();
 
+      final result = await directions.directionsWithLocation(
+        Location(lat: routeDetails.startLocation.latitude, lng: routeDetails.startLocation.longitude),
+        Location(lat: routeDetails.endLocation.latitude, lng: routeDetails.endLocation.longitude),
+        waypoints: waypoints,
+      );
 
-    final result = await directions.directionsWithLocation(
-      Location(lat: routeDetails.startLocation.latitude, lng: routeDetails.startLocation.longitude),
-      Location(lat: routeDetails.endLocation.latitude, lng: routeDetails.endLocation.longitude),
-      waypoints: waypoints, // Add the waypoints here
-    );
-    
+      if (result.isOkay) {
+        List<LatLng> routeCoords = _decodePolyline(result.routes[0].overviewPolyline.points);
 
-    if (result.isOkay) {
-      setState(() {
-        _routeCoords.addAll(_decodePolyline(result.routes[0].overviewPolyline.points));
-        _polylines.add(Polyline(
-          polylineId: PolylineId("route_$i"),
-          visible: true,
-          points: _routeCoords,
-          color: Colors.blue,
-          width: 4,
-        ));
-      });
+        setState(() {
+          _polylines.add(Polyline(
+            polylineId: PolylineId("route_$i"),
+            visible: true,
+            points: routeCoords,
+            color: Colors.blue,
+            width: 4,
+          ));
+        });
+      }
     }
   }
-}
-  
+
   List<LatLng> _decodePolyline(String encoded) {
     List<LatLng> polyline = [];
     int index = 0, len = encoded.length;
@@ -132,59 +162,59 @@ class _MyAppState extends State<MyApp>{
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Google Maps in Flutter'),
-          //backgroundColor: Colors.green[700],
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
             target: _center,
-            zoom: 13.0
-            ),
-            markers: _createMarkers(), // Use a method to combine all markers
-          polylines: _polylines, // Display the polyline on the map
+            zoom: 13.0,
+          ),
+          markers: _createMarkers(),
+          polylines: _polylines,
         ),
       ),
     );
   }
-   Set<Marker> _createMarkers() {
+
+  Set<Marker> _createMarkers() {
     return {
-              const Marker(
-                markerId: MarkerId('KDU'),
-                position: LatLng(6.817801415099614, 79.89069891600366),
-                infoWindow: InfoWindow(title: "KDU")
-              ),
-              const Marker(
-                markerId: MarkerId('UHKDU'),
-                position: LatLng(6.825240929708998, 79.9082921331989),
-                infoWindow: InfoWindow(title: "UHKDU")
-              ),
-              createMarker('Panadura', const LatLng(6.7104892147864605, 79.90773987399923),"120.00"),
-              createMarker('Bandaragama', const LatLng(6.714151106845103, 79.99073558146642),"180.00"),
-              createMarker('Horana', const LatLng(6.723170703502162, 80.06583043717407),"220.00"),
-              createMarker('Piliyandala', const LatLng(6.801664896754592, 79.92279155884555),"60.00"),
-              createMarker('Kottawa', const LatLng(6.841110311554549, 79.96538119310712),"150.00"),
-              createMarker('Godagama', const LatLng(6.851902488708469, 80.03334731284404),"200.00"),
-              createMarker('Pahathgama', const LatLng(6.896416594616102, 80.08231467508558),"250.00"),
-              createMarker('Maharagama', const LatLng(6.850763264031758, 79.92749206117588),"70.00"),
-              createMarker('Koswatta', const LatLng(6.908986729283659, 79.93080459154916),"120.00"),
-              createMarker('Kaduwela', const LatLng(6.928927044316318, 79.9826355994138),"160.00"),
-              createMarker('Delgoda', const LatLng(6.988008813288438, 80.0159422824388),"200.00"),
-              createMarker('Weliweriya', const LatLng(7.0319529504541665, 80.02978339087662),"220.00"),
-              createMarker('Yakkala', const LatLng(7.086412609614552, 80.03433536285229),"260.00"),
-              createMarker('Nittambuwa', const LatLng(7.142191208759358, 80.10636908834084),"300.00"),
-              createMarker('Kesbewa', const LatLng(6.7781983860413995, 79.9476704429502),"60.00"),
-              createMarker('Polgasowita/Kindelpitiya', const LatLng(6.788272881361812, 79.99172337450369),"110.00"),
-              createMarker('Gonapola/Gammanpila', const LatLng(6.755930079059569, 80.0166182135967),"150.00"),
-              createMarker('Kumbuka/Bandaragama', const LatLng(6.736008281612015, 80.02806926808988),"170.00"),
-              createMarker('Pokunuwita', const LatLng(6.72902450012628, 80.03227120891174),"190.00"),
-              createMarker('Aththidiya', const LatLng(6.8397971035243135, 79.88561675594863),"40.00"),
-              createMarker('Nugegoda', const LatLng(6.865207771841957, 79.89794010974578),"70.00"),
-              createMarker('Peliyagoda', const LatLng(6.9589941781914835, 79.89336981001003),"170.00"),
-              createMarker('Ja-Ela', const LatLng(7.067703587465936, 79.90154521053142),"250.00"),
-              createMarker('Negambo', const LatLng(7.207330829673679, 79.85258837399863),"290.00"),
-          };
-          
+      const Marker(
+        markerId: MarkerId('KDU'),
+        position: LatLng(6.817801415099614, 79.89069891600366),
+        infoWindow: InfoWindow(title: "KDU"),
+      ),
+      const Marker(
+        markerId: MarkerId('UHKDU'),
+        position: LatLng(6.825240929708998, 79.9082921331989),
+        infoWindow: InfoWindow(title: "UHKDU"),
+      ),
+      createMarker('Panadura', const LatLng(6.7104892147864605, 79.90773987399923), "120.00"),
+      createMarker('Bandaragama', const LatLng(6.714151106845103, 79.99073558146642), "180.00"),
+      createMarker('Horana', const LatLng(6.723170703502162, 80.06583043717407), "220.00"),
+      createMarker('Piliyandala', const LatLng(6.801664896754592, 79.92279155884555), "60.00"),
+      createMarker('Kottawa', const LatLng(6.841110311554549, 79.96538119310712), "150.00"),
+      createMarker('Godagama', const LatLng(6.851902488708469, 80.03334731284404), "200.00"),
+      createMarker('Pahathgama', const LatLng(6.896416594616102, 80.08231467508558), "250.00"),
+      createMarker('Maharagama', const LatLng(6.850763264031758, 79.92749206117588), "70.00"),
+      createMarker('Koswatta', const LatLng(6.908986729283659, 79.93080459154916), "120.00"),
+      createMarker('Kaduwela', const LatLng(6.928927044316318, 79.9826355994138), "160.00"),
+      createMarker('Delgoda', const LatLng(6.988008813288438, 80.0159422824388), "200.00"),
+      createMarker('Weliweriya', const LatLng(7.0319529504541665, 80.02978339087662), "220.00"),
+      createMarker('Yakkala', const LatLng(7.086412609614552, 80.03433536285229), "260.00"),
+      createMarker('Nittambuwa', const LatLng(7.142191208759358, 80.10636908834084), "300.00"),
+      createMarker('Kesbewa', const LatLng(6.7781983860413995, 79.9476704429502), "60.00"),
+      createMarker('Polgasowita/Kindelpitiya', const LatLng(6.788272881361812, 79.99172337450369), "110.00"),
+      createMarker('Gonapola/Gammanpila', const LatLng(6.755930079059569, 80.0166182135967), "150.00"),
+      createMarker('Kumbuka/Bandaragama', const LatLng(6.736008281612015, 80.02806926808988), "170.00"),
+      createMarker('Pokunuwita', const LatLng(6.72902450012628, 80.03227120891174), "190.00"),
+      createMarker('Aththidiya', const LatLng(6.8397971035243135, 79.88561675594863), "40.00"),
+      createMarker('Nugegoda', const LatLng(6.865207771841957, 79.89794010974578), "70.00"),
+      createMarker('Peliyagoda', const LatLng(6.9589941781914835, 79.89336981001003), "170.00"),
+      createMarker('Ja-Ela', const LatLng(7.067703587465936, 79.90154521053142), "250.00"),
+      createMarker('Negambo', const LatLng(7.207330829673679, 79.85258837399863), "290.00"),
+    };
   }
+
   Marker createMarker(String markerId, LatLng position, String price) {
     return Marker(
       markerId: MarkerId(markerId),
