ede47374eb88900ee7bba611f05acf74e9a4acea
MasterJeon <165636159+MasterJeon@users.noreply.github.com>
2024-12-02 05:57:57 +0530

    E wallet edits

--- lib/home/myWallet/eWallet.dart
+++ lib/home/myWallet/eWallet.dart
@@ -87,16 +87,6 @@ class EWalletState extends State<EWallet> {
               child: Column(
                 children: [
                   // Wallet Balance Container
-                  SizedBox(
-                    width: double.infinity,
-                    child: Container(
-                      padding: EdgeInsets.all(28),
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 77, 157, 255),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Column(
-                        children: [
                           Text(
                             'E-Wallet',
                             style: TextStyle(
@@ -127,11 +117,7 @@ class EWalletState extends State<EWallet> {
                         ],
                       ),
                     ),
-                  ),
                   SizedBox(height: 48),
-                ],
-              ),
-            ),
             // White Box Containing Buttons
             Expanded(
               child: Container(
@@ -143,10 +129,11 @@ class EWalletState extends State<EWallet> {
                     topRight: Radius.circular(30),
                   ),
                 ),
-                padding: EdgeInsets.all(16),
+                padding: EdgeInsets.all(20),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
+                    SizedBox(height: 20),
                     profileTab(
                       "Recharge Balance",
                       onTap: () {
@@ -156,7 +143,7 @@ class EWalletState extends State<EWallet> {
                         );
                       },
                     ),
-                    SizedBox(height: 16),
+                    SizedBox(height: 20),
                     profileTab(
                       "Refund",
                       onTap: () {
